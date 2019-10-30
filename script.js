4db0224cd11f4f0c2fcc8312f5bc27a10336cd02
Garrett Faucher <GarrettFaucher@users.noreply.github.com>
2019-10-30 18:29:08 -0400

    Stores data in chrome storage

--- script.js
+++ script.js
@@ -10,32 +10,27 @@ window.addEventListener('load', function load(event){
 // collectData retrieves the filled in text boxes from the HTML display and
 // populate input.json with the data gathered.
 function collectData() {
-  console.log("collectData called.");
-  var crn_1 = document.getElementById('crn_1').value;
-  var crn_2 = document.getElementById('crn_2').value;
-  var crn_3 = document.getElementById('crn_3').value;
-  var crn_4 = document.getElementById('crn_4').value;
-  var crn_5 = document.getElementById('crn_5').value;
-  var crn_6 = document.getElementById('crn_6').value;
-  var crn_7 = document.getElementById('crn_7').value;
-  var crn_8 = document.getElementById('crn_8').value;
-  var username = document.getElementById('username').value;
-  var password = document.getElementById('password').value;
-  var data = {crn_1: crn_1,
-              crn_2: crn_2,
-              crn_3: crn_3,
-              crn_4: crn_4,
-              crn_5: crn_5,
-              crn_6: crn_6,
-              crn_7: crn_7,
-              crn_8: crn_8,
-              username: username,
-              password: password
-            };
+     console.log("collectData called.");
+     // Store the data from each textbox
+     var crn_1 = document.getElementById('crn_1').value;
+     var crn_2 = document.getElementById('crn_2').value;
+     var crn_3 = document.getElementById('crn_3').value;
+     var crn_4 = document.getElementById('crn_4').value;
+     var crn_5 = document.getElementById('crn_5').value;
+     var crn_6 = document.getElementById('crn_6').value;
+     var crn_7 = document.getElementById('crn_7').value;
+     var crn_8 = document.getElementById('crn_8').value;
+     var username = document.getElementById('username').value;
+     var password = document.getElementById('password').value;
 
-  console.log(data);
-  var jsonData = JSON.stringify(data),
-       filename = 'input.json',
-       blob = new Blob([jsonData], {type: "text/plain;charset=utf-8"});
-  // saveAs(blob, filename); // This might not be needed I don't remember
+     // Put it in a vector to loop through
+     var crnData = [crn_1, crn_2, crn_3, crn_4, crn_5, crn_6, crn_7, crn_8, username, password];
+     console.log(crnData); // Show the data for bug fixing
+     // Loop through vector and stor it in local Chrome storage
+     for(var i = 0; i < crnData.length; i++) {
+          var currentKey = "crn_" + (i+1);
+          chrome.storage.local.set({currentKey: crnData[i]}, (function(crnData) {
+               console.log('Value is set to ' + crnData[i]);
+          })(crnData));
+     }
 }
