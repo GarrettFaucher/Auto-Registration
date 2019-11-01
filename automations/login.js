7e52ccdba8dfb9acae588a8e5f3a6caca655be6c
Patrick Dundas <patrickdundas@users.noreply.github.com>
2019-11-01 14:59:23 -0400

    Autofill credentials

--- automations/login.js
+++ automations/login.js
@@ -1,4 +1,12 @@
 /*
-  Stage 4 of tabDriver.js
+  Stage 1 of tabDriver.js
   Fill and submit the login form on uvm.edu
 */
+
+console.log("running login automation on login.js");
+
+var usernameInput = document.getElementsByName("username")[0];
+var passwordInput = document.getElementsByName("password")[0];
+
+usernameInput.value="username";
+passwordInput.value="password";
