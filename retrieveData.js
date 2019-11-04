b0d97c2eb431950da56bf6c42bc12abc35310e72
Garrett Faucher <GarrettFaucher@users.noreply.github.com>
2019-11-04 18:23:24 -0500

    Store time and date

--- retrieveData.js
+++ retrieveData.js
@@ -14,6 +14,9 @@ function collectData() {
      // Store the data from each textbox
      var username = document.getElementById('username').value;
      var password = document.getElementById('password').value;
+     var date = document.getElementById('reg_date').value; // Date stored in format YYYY-MM-DD
+     // Possible values for time: 600am, 630am, 700am
+     var time = document.getElementById('reg_time').value;
      var crn_1 = document.getElementById('crn_1').value;
      var crn_2 = document.getElementById('crn_2').value;
      var crn_3 = document.getElementById('crn_3').value;
@@ -24,19 +27,20 @@ function collectData() {
      var crn_8 = document.getElementById('crn_8').value;
 
      // Put it in a vector to loop through
-     var crnData = [username, password, crn_1, crn_2, crn_3, crn_4, crn_5, crn_6, crn_7, crn_8];
+     var crnData = [username, password, date, time, crn_1, crn_2, crn_3, crn_4, crn_5, crn_6, crn_7, crn_8];
      console.log(crnData); // Show the data for bug fixing
      // Loop through vector and store it in local Chrome storage
+     var currentKey;
      for(var i = 0; i < crnData.length; i++) {
-          if (i == 0 || i == 1) {
-               if (i == 0) {
-                    var currentKey = "username";
-               } else {
-                    var currentKey = "password";
-               }
+          if (i <= 3) {
+               if (i == 0)      {currentKey = "username";}
+               else if (i == 1) {currentKey = "password";}
+               else if (i == 2) {currentKey = "date";}
+               else if (i == 3) {currentKey = "time";}
           } else {
-               var currentKey = "crn_" + (i-1);
+               currentKey = "crn_" + (i - 1);
           }
+          // Storing the data in local chrome storage
           chrome.storage.local.set({currentKey: crnData[i]}, (function(crnData) {
                console.log(currentKey + ' is set to ' + crnData[i]);
           })(crnData));
