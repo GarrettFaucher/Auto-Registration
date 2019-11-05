4a879c21c8e1e791dee62966546fcce683b69bc6
Patrick Dundas <patrickdundas@users.noreply.github.com>
2019-11-05 16:55:47 -0500

    Improved comments again

--- background.js
+++ background.js
@@ -1,13 +1,24 @@
+// background.js runs continually in the background of our extension
+// and is not limited to the scope of the popup, and does not depend on the popup being visible
+
+//runClick is called when the "Run" button is clicked in the popup
+//function is executed when our listener detects "runClick" event sent by popup.js
 function runClick(){
-  console.log("running runclick")
+  console.log("Run button was clicked, beginning runClick()")
   chrome.tabs.create({
     url: 'http://myuvm.uvm.edu'
   }, function(tab){
-    //send message saying to start
-    console.log("tab created, waiting 2 seconds to send command")
+    //once the tab is created, but not neccesarily loaded
+    console.log("tab created, waiting 2 seconds to start tabDriver")
+
+    //set a timeout to execute start command after 2 seconds
     setTimeout(function(){
-      console.log("sending start command to driver")
+      console.log("Firing up tabDriver")
+      //get listing of tabs
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+          //send a message to the active tab with command = start
+          //the active tab should be myuvm.uvm.edu since we just created that
+          //this tab will have tabDriver injected into it, so it will be waiting for a command
           chrome.tabs.sendMessage(tabs[0].id, {command: "start"}, function(response) {});
       });
     },2000);
