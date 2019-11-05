f126cf99de33de9a2db67336578db57d3a097378
Garrett Faucher <GarrettFaucher@users.noreply.github.com>
2019-11-04 19:57:03 -0500

    Generic login working

--- automations/login.js
+++ automations/login.js
@@ -3,10 +3,26 @@
   Fill and submit the login form on uvm.edu
 */
 
-console.log("running login automation on login.js");
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-var usernameInput = document.getElementsByName("username")[0];
-var passwordInput = document.getElementsByName("password")[0];
+async function login() {
+     console.log("running login automation on login.js");
 
-usernameInput.value="username";
-passwordInput.value="password";
+     var usernameInput = document.getElementsByName("username")[0];
+     var passwordInput = document.getElementsByName("password")[0];
+     var submitButton = document.getElementsByName("submit")[0];
+
+     // TODO: Use chrome.local data to fill properly
+     usernameInput.value="username";
+     passwordInput.value="password";
+
+     console.log('Waiting for page to load...');
+     await sleep(2000);
+     console.log('Page should have loaded.');
+     submitButton.click();
+
+}
+
+login();
